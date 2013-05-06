26dd25fb51efea9d4dee31f8a824eacb4ae41820
Louis Chatriot <louis.chatriot@gmail.com>
2013-05-06 18:56:22 +0200

    Package.json, deps, store are done

---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ * Session store for Connect/Express, backed by nedb
+ *
+ * Usage:
+ * var connect = require('connect')
+ *   , NedbStore = require('connect-nedb-session')(connect);
+ *
+ * Or:
+ * var express = require('express')
+ *   , NedbStore = require('connect-nedb-session')(express);
+ *
+ * Then:
+ * expressServer.use(express.session({ secret: 'yoursecret'
+ *                                   , key: 'yoursessionkey'
+ *                                   , cookie: { path: '/'
+ *                                             , httpOnly: true
+ *                                             , maxAge: 365 * 24 * 3600 * 1000   // One year for example
+ *                                             }
+ *                                   , store: new NedbStore({ filename: 'path_to_nedb_persistence_file' })
+ *                                   }));
+ */
+var Nedb = require('nedb');
+
+
+module.exports = function (connect) {
+  /**
+   * Constructor
+   * @param {String} options.filename File where session data will be persisted
+   */
+  function NedbStore(options) {
+    this.filename = options.filename;
+    this.db = new Nedb(options.filename);
+    this.db.loadDatabase();   // Asynchronous but very quick
+  }
+
+  // Inherit from Connect's session store
+  NedbStore.prototype.__proto__ = connect.session.Store.prototype;
+
+
+  /**
+   * Get session data
+   */
+  NedbStore.prototype.get = function (sid, callback) {
+    this.db.findOne({ sid: sid }, function (err, sess) {
+      if (err) { return callback(err); }
+      if (!sess) { return callback(null, null); }
+
+      return callback(null, sess.data);
+    });
+  };
+
+
+  /**
+   * Set session data
+   */
+  NedbStore.prototype.set = function (sid, data, callback) {
+    this.db.update({ sid: sid }, { sid: sid, data: data }, { multi: false, upsert: true }, function (err, ne, up) {
+      return callback(err);
+    });
+  };
+
+
+  /**
+   * Destroy a session's data
+   */
+  NedbStore.prototype.destroy = function (sid, callback) {
+    this.db.remove({ sid: sid }, { multi: false }, function (err) {
+      return callback(err);
+    });
+  };
+
+
+  return NedbStore;
+};
